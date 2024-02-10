545c6b82b8eccbb881092e72614f7ed258abe15f
Pol Gubau <gubaupol@gmail.com>
2024-02-10 13:04:13 +0100

    Update component summaries***

---
+++ examples/Banner/root.tsx
@@ -0,0 +1,13 @@
+const code = `
+import { Banner } from "pol-ui";
+import React from "react";
+const BannerComponent = () => {
+  return (
+    <Banner>
+      <span>Welcome back to Pol-ui</span>
+    </Banner>
+  );
+};
+export default BannerComponent;
+`;
+export default code;
