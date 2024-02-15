802b6bc33b96c14ef400af5b521fb18f41a8c0f8
Pol Gubau <gubaupol@gmail.com>
2024-02-15 21:25:23 +0100

    Update BubbleHeading component imports and add customization options

---
+++ examples/BubbleHeading/dark.tsx
@@ -0,0 +1,13 @@
+const code = `
+import { Breadcrumb, BreadcrumbItem } from "pol-ui";
+import React from "react";
+const BubbleHeadingComponent = () => {
+  return (
+    <div className="dark bg-secondary-900 p-5 rounded-xl">
+    <BubbleHeading>Pol-ui, powering web development</BubbleHeading>
+  </div>
+  );
+};
+export default BubbleHeadingComponent;
+`;
+export default code;
