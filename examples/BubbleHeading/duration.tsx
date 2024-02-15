ee7f7d2441bf5bf1b0b23eac6dbe37d443836bab
Pol Gubau <gubaupol@gmail.com>
2024-02-15 17:32:41 +0100

    Fix import casing in carousel.en-US.mdx

---
+++ examples/BubbleHeading/duration.tsx
@@ -0,0 +1,11 @@
+const code = `
+import { Breadcrumb, BreadcrumbItem } from "pol-ui";
+import React from "react";
+const BubbleHeadingComponent = () => {
+  return (
+    <BubbleHeadingProps transitionDuration={0.7}>Pol-ui, powering web development</BubbleHeadingProps>
+  );
+};
+export default BubbleHeadingComponent;
+`;
+export default code;
---
+++ examples/BubbleHeading/root.tsx
@@ -0,0 +1,11 @@
+const code = `
+import { Breadcrumb, BreadcrumbItem } from "pol-ui";
+import React from "react";
+const BubbleHeadingComponent = () => {
+  return (
+    <BubbleHeadingProps>Pol-ui, powering web development</BubbleHeadingProps>
+  );
+};
+export default BubbleHeadingComponent;
+`;
+export default code;
