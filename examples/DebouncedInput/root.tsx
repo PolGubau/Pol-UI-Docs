5ef13ed45a9e55469a37bf390e11e424c327e3b0
Pol Gubau <gubaupol@gmail.com>
2024-02-16 14:38:35 +0100

    Refactor code for improved performance

---
+++ examples/DebouncedInput/root.tsx
@@ -0,0 +1,23 @@
+const code = `
+import { DebouncedInput } from "pol-ui";
+
+import React from "react";
+
+const DebouncedInputComponent = () => {
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <DebouncedInput
+        label="The returned value is debounced by 500ms (default)"
+        onChange={(v) => setValue(e)} // <- The parameter is directly the value
+        placeholder="Type something..."
+        value=""
+      />
+      <p className="mt-4">Value: {value}</p>
+    </>
+  );
+};
+export default DebouncedInputComponent;
+`;
+export default code;
