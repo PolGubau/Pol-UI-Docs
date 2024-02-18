aa43df7677ad960e2791c88085db22f23ca1dfb1
Pol Gubau <gubaupol@gmail.com>
2024-02-18 22:03:17 +0100

    Update import paths for IconButton component

---
+++ examples/IconButton/outline.tsx
@@ -0,0 +1,15 @@
+const code = `
+import { IconButton } from "pol-ui";
+import { TbSearch } from "react-icons/tb";
+import React from "react";
+
+const IconButtonComponent = () => {
+  return (
+    <IconButton outline>
+      <TbSearch />
+    </IconButton>
+  );
+};
+export default IconButtonComponent;
+`;
+export default code;
