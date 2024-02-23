9a98e4559031d3e49a31ae3bbab9efccbf05e301
Pol Gubau <gubaupol@gmail.com>
2024-02-23 20:06:28 +0100

    Update ParallaxText component documentation

---
+++ examples/PasswordInput/root.tsx
@@ -0,0 +1,13 @@
+const code = `
+import { PasswordInput } from "pol-ui";
+
+const PasswordInputComponent = () => {
+  const text = 'Parallax text in Pol-ui'
+
+  return (
+    <PasswordInput placeholder="******" label="Password" />
+  );
+};
+export default PasswordInputComponent;
+`;
+export default code;
